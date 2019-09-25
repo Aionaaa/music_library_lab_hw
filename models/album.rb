5e305b7b239f4a52d0335cbc7ccf31f8b04388a8
Iona Grant <ionagrant92@gmail.com>
2019-09-25 16:10:39 +0100

    completed delete and update

--- models/album.rb
+++ models/album.rb
@@ -4,26 +4,50 @@ require_relative("artist")
 
 class Album
 
-attr_reader(:id, :artist_id)
-attr_accessor(:name, :genre)
+  attr_reader(:id, :artist_id)
+  attr_accessor(:name, :genre)
 
-def initialize( options )
-  @id = options['id'].to_i() if options['id']
-  @name = options['name']
-  @genre = options['genre']
-  @artist_id = options['artist_id'].to_i()
-end
+  def initialize( options )
+    @id = options['id'].to_i() if options['id']
+    @name = options['name']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i()
+  end
 
-def save()
-  sql = "INSERT INTO albums(
-  name,
-  genre,
-  artist_id
-  ) VALUES ($1, $2, $3)
-  RETURNING id"
-  values = [@name, @genre, @artist_id]
-  @id = SqlRunner.run(sql, values)[0]['id'].to_i()
-end
+  def save()
+    sql = "INSERT INTO albums(
+    name,
+    genre,
+    artist_id
+    ) VALUES ($1, $2, $3)
+    RETURNING id"
+    values = [@name, @genre, @artist_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i()
+  end
+
+  def update()
+    sql = "UPDATE albums SET (
+    name,
+    genre
+    ) = ($1, $2) WHERE id = $3"
+    values = [@name, @genre, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM albums WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM albums WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    album_hash = results.first
+    album = Album.new(album_hash)
+    return album
+  end
 
   def self.delete_all()
     sql = "DELETE FROM albums"
--- models/artist.rb
+++ models/artist.rb
@@ -4,7 +4,8 @@ require_relative("album")
 
 class Artist
 
-  attr_reader(:id, :name)
+  attr_reader(:id)
+  attr_accessor(:name)
 
   def initialize ( options )
     @id = options['id'].to_i() if options['id']
@@ -15,6 +16,31 @@ class Artist
     sql = "INSERT INTO artists (name) VALUES ($1) RETURNING id"
     values = [@name]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i()
+  end
+
+  def update()
+    sql = "UPDATE artists SET 
+    name
+     = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+
+  def delete()
+    sql = "DELETE FROM artists WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+
+  def self.find(artist_id)
+    sql = "SELECT * FROM artists WHERE id = $1"
+    values = [artist_id]
+    results = SqlRunner.run(sql, values)
+    artist_hash = results.first
+    artist = Artist.new(artist_hash)
+    return artist
   end
 
   def albums()
